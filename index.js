344136285765b7c089bfaad9a89f0aa1ebb2d617
colormaa <colormaa@gmail.com>
2022-09-03 20:11:21 +0800

    Fdsafad

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var app = express();
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.get("/", async(req, res)=>{
+    res.json({"message":"hi tehre"})
+})
 app.get('/getWord', async(req, res)=>{
     
     if(req.query.word!=null ||req.query.word!==undefined){
